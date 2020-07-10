65539edf230c2c74d4b43922eb61401e7a3d1c44
Jesus Matos <jesus.matosp@gmail.com>
2020-07-09 22:44:32 -0500

    first commit

---
+++ src/test/java/com/jmatos/docker/JmatosSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmatos.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmatosSpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
